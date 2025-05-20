4c3cf4870f7a0069405cab98ab2eb1229fe63e5c
Cyliann <maksymilian@cych.eu>
2025-05-20 22:48:58 +0200

    feat: add logic to parse conversations from neo4j response

--- main.py
+++ main.py
@@ -1,7 +1,9 @@
 from dataclasses import dataclass
-from typing import Self
+from typing import Any, NotRequired, TypedDict, get_type_hints
 from datetime import date, timedelta
 import time
+
+from neo4j import Record
 
 import db
 
@@ -9,18 +11,63 @@ import db
 @dataclass
 class Tweet:
     id: str
-    labels: list[str]
     sentiment_label: str
     created_at: date
-    reply_to: Self
+    negative: float
+    neutral: float
+    positive: float
+    reply_to: str | None = None
+
+
+class TweetData(TypedDict):
+    id: str
+    sentiment_label: str
+    created_at: date
+    negative: float
+    neutral: float
+    positive: float
+    reply_to: NotRequired[str]
+
+
+def make_tweet(data: TweetData) -> Tweet:
+    return Tweet(**data)
+
+
+def filter_to_tweet_fields(
+    data: dict[Any, Any],  # pyright: ignore[reportExplicitAny]
+) -> dict[str, Any]:  # pyright: ignore[reportExplicitAny]
+    allowed_keys = get_type_hints(TweetData).keys()
+    return {k: data[k] for k in allowed_keys if k in data}
+
+
+def make_conversation(record: Record) -> list[Tweet]:
+    conversation: list[Tweet] = []
+    data = record.data()
+
+    parent_data: TweetData = filter_to_tweet_fields(data["parent"])  # pyright: ignore[reportAny, reportAssignmentType]
+    parent = make_tweet(parent_data)
+    conversation.append(parent)
+
+    tree_nodes = data["tree_nodes"]  # pyright: ignore[reportAny]
+    for node in tree_nodes:  # pyright: ignore[reportAny]
+        reply_data: TweetData = filter_to_tweet_fields(node)  # pyright: ignore[reportAny, reportAssignmentType]
+        reply = make_tweet(reply_data)
+        conversation.append(reply)
+
+    return conversation
 
 
 def main():
+    conversations: list[list[Tweet]] = []
     start_time = time.time()
     records = db.get_conversations()
-    end_time = time.time()
-    time_spent = end_time - start_time
-    print(f"Time taken: {str(timedelta(seconds=time_spent))}")
+    print(f"Time taken: {str(timedelta(seconds=time.time() - start_time))}")
+
+    for record in records:
+        conversations.append(make_conversation(record))
+
+    print(f"Number of conversations: {len(conversations)}")
+
 
 
 if __name__ == "__main__":
