99ebf1c893fb02f96a7c12955fa7a5a171d3034c
Cyliann <maksymilian@cych.eu>
2025-05-20 20:45:11 +0200

    refactor: simplify conversation fetch query

--- db.py
+++ db.py
@@ -17,15 +17,9 @@ WITH root_airline_tweet, collect(path) AS paths, parent, r
 
 // Flatten paths to get all nodes and relationships
 WITH root_airline_tweet, parent, r,
-     apoc.coll.toSet(apoc.coll.flatten([p IN paths | nodes(p)])) AS tree_nodes,
-     apoc.coll.toSet(apoc.coll.flatten([p IN paths | relationships(p)])) AS tree_rels
+     apoc.coll.toSet(apoc.coll.flatten([p IN paths | nodes(p)])) AS tree_nodes
 
-// WHERE size(tree_nodes) > 1
-
-RETURN parent, r, root_airline_tweet, tree_nodes, tree_rels, size(tree_nodes) AS num_nodes
-// count number of conversations-trees (roots)
-// RETURN COUNT(root_airline_tweet) AS conversations, SUM(size(tree_nodes)) AS num_tweets_in_such_conversations
-limit 10
+RETURN parent, tree_nodes, size(tree_nodes+1) AS num_nodes
 """
 
 
@@ -44,16 +38,8 @@ def run_query(driver: neo4j.Driver, query: LiteralString) -> list[neo4j.Record]:
         database_="neo4j",
     )
 
-    # Loop through results and do something with them
-    for tweet in records:
-        print(tweet)
-
     # Summary information
     print(
-        "The query `{query}` returned {records_count} records in {time} ms.".format(
-            query=summary.query,
-            records_count=len(records),
-            time=summary.result_available_after,
-        )
+        f"The query returned {len(records)} records in {summary.result_available_after} ms."
     )
     return records
